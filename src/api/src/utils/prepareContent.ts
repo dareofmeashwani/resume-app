b0c107a74474aa2d456a02083a062a304fb19409
I349391 <a.kumar.verma@sap.com>
2023-01-25 14:25:19 +0530

    fix: tweak download file implementation

--- src/api/src/utils/prepareContent.ts
+++ src/api/src/utils/prepareContent.ts
@@ -3,25 +3,27 @@ import fs from "fs";
 import https from "https";
 import path from "path";
 import admZip from "adm-zip";
+import axios from "axios";
 function downloadFile(url: string, filename: string) {
 	const file = fs.createWriteStream(filename);
 	return new Promise((resolve, reject) => {
 		const download = (url: string) => {
-			https
-				.get(url, (response: any) => {
+			axios
+				.get(url, { responseType: "stream" })
+				.then((response: any) => {
 					if (response.statusCode == 302) {
 						download(response.headers.location);
 					} else {
-						response.pipe(file);
+						response.data.pipe(file);
 						file.on("finish", () => {
-							file.close(()=>{
+							file.close(() => {
 								console.log("Download Completed");
 								resolve(null);
 							});
 						});
 					}
 				})
-				.on("error", (err) => {
+				.catch((err) => {
 					reject(err);
 				});
 		};
@@ -36,6 +38,6 @@ export default async function downloadContent() {
 	const filePath = path.join(__dirname, "../temp.zip");
 	console.log(filePath);
 	await downloadFile(config.CONTENT_URL, filePath);
-	//unzipFile(filePath,path.join(__dirname, "../"));
-	//fs.unlinkSync(filePath);
+	unzipFile(filePath,path.join(__dirname, "../"));
+	fs.unlinkSync(filePath);
 }
