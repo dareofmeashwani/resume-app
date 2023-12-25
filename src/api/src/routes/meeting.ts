4c7ed5eccba1619754895bff55e9fc6b5bde6cd0
Ashwani Kumar Verma <a.kumar.verma@sap.com>
2023-12-26 00:43:07 +0530

    fix: log cancellation data

--- src/api/src/routes/meeting.ts
+++ src/api/src/routes/meeting.ts
@@ -103,61 +103,6 @@ export const patchMeeting = async (
 		res.status(HTTP_STATUS.ACCEPTED).send(getProps(response._doc));
 	}
 };
-export const deleteMeeting = async (
-	req: express.Request,
-	res: express.Response
-) => {
-	let admins = [];
-	let response;
-	try {
-		admins = await User.find({ role: ROLES.ADMIN });
-		response = await meetingModel.findOne({
-			_id: req.params.meetingId,
-			createdBy: res.locals.userData.email
-		});
-	} catch (error) {
-		throwResumeError(
-			HTTP_STATUS.SERVICE_UNAVAILABLE,
-			ERROR_MESSAGES.DB_CONNECTIVITY_ERROR,
-			req,
-			error
-		);
-	}
-	if (!response) {
-		throwResumeError(HTTP_STATUS.NOT_FOUND, ERROR_MESSAGES.NOT_FOUND_ERROR, req);
-	}
-	let attendees: any = unique([
-		...response._doc.members,
-		res.locals.userData.email,
-		admins.map((admin) => admin.email)
-	]);
-	let zoomData;
-	if (response._doc.externalEventId) {
-		/*try {
-			zoomData = await zoom.get(response._doc.externalEventId);
-			await zoom.del(response._doc.externalEventId);
-		} catch (error) {
-			throwResumeError(
-				HTTP_STATUS.INTERNAL_SERVER_ERROR,
-				ERROR_MESSAGES.CALENDLY_CONNECTIVITY_ERROR,
-				req,
-				error
-			);
-		}*/
-	}
-	try {
-		await meetingModel.deleteOne({ _id: req.params.meetingId });
-		res.status(HTTP_STATUS.ACCEPTED).send();
-	} catch (error) {
-		throwResumeError(
-			HTTP_STATUS.SERVICE_UNAVAILABLE,
-			ERROR_MESSAGES.DB_CONNECTIVITY_ERROR,
-			req,
-			error
-		);
-	}
-	//sendZoomCancellation(res.locals.userData, attendees, zoomData?.data, response);
-};
 
 
 export async function resendInvite(
@@ -202,7 +147,6 @@ export async function resendInvite(
 }
 
 export async function createCalendlyMeeting(data: any) {
-	console.log(JSON.stringify(data));
 	const payload = data.payload;
 	const scheduledEvent = data.payload.scheduled_event;
 	const admins = await User.find({ role: ROLES.ADMIN });
@@ -288,6 +232,7 @@ export const getMeeting = async function (
 };
 
 export async function cancelCalendlyMeeting(data: any) {
+	console.log(JSON.stringify(data));
 	//const data_cancelled = {"created_at":"2023-12-24T17:55:40.000000Z","created_by":"https://api.calendly.com/users/f2f32253-5f99-4d22-8950-51f86535e227","event":"invitee.canceled","payload":{"cancel_url":"https://calendly.com/cancellations/3e39fe12-9338-448d-b760-2217a35cc0a7","cancellation":{"canceled_by":"Ashwani Kumar Verma","canceler_type":"host","created_at":"2023-12-24T17:55:39.915208Z","reason":""},"created_at":"2023-12-24T17:27:38.192438Z","email":"ashwaniverma.verma@rediff.com","event":"https://api.calendly.com/scheduled_events/9a15cc4a-c99d-48e4-bc2b-b363b35c669c","first_name":null,"invitee_scheduled_by":null,"last_name":null,"name":"test account","new_invitee":null,"no_show":null,"old_invitee":null,"payment":null,"questions_and_answers":[],"reconfirmation":null,"reschedule_url":"https://calendly.com/reschedulings/3e39fe12-9338-448d-b760-2217a35cc0a7","rescheduled":false,"routing_form_submission":null,"scheduled_event":{"cancellation":{"canceled_by":"Ashwani Kumar Verma","canceler_type":"host","created_at":"2023-12-24T17:55:39.915208Z","reason":""},"created_at":"2023-12-24T17:27:38.162964Z","end_time":"2023-12-24T21:45:00.000000Z","event_guests":[],"event_memberships":[{"user":"https://api.calendly.com/users/f2f32253-5f99-4d22-8950-51f86535e227","user_email":"connect2ashwaniverma@gmail.com"}],"event_type":"https://api.calendly.com/event_types/ebd636e1-bf06-4b5b-a3f8-68add9964c7a","invitees_counter":{"total":1,"active":0,"limit":100},"location":{"join_url":"https://calendly.com/events/9a15cc4a-c99d-48e4-bc2b-b363b35c669c/google_meet","status":"pushed","type":"google_conference"},"name":"15 Minute Meeting","start_time":"2023-12-24T21:30:00.000000Z","status":"canceled","updated_at":"2023-12-24T17:55:39.924665Z","uri":"https://api.calendly.com/scheduled_events/9a15cc4a-c99d-48e4-bc2b-b363b35c669c"},"scheduling_method":null,"status":"canceled","text_reminder_number":null,"timezone":"Asia/Calcutta","tracking":{"utm_campaign":null,"utm_source":null,"utm_medium":null,"utm_content":null,"utm_term":null,"salesforce_uuid":null},"updated_at":"2023-12-24T17:55:39.938754Z","uri":"https://api.calendly.com/scheduled_events/9a15cc4a-c99d-48e4-bc2b-b363b35c669c/invitees/3e39fe12-9338-448d-b760-2217a35cc0a7"}};
 }
 
