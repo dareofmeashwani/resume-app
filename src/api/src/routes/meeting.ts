d714bfe6f7a79cc90e4fed93d417b0d84a563f75
Ashwani Kumar Verma <a.kumar.verma@sap.com>
2023-12-26 11:53:35 +0530

    fix: webhook cancel event handling

--- src/api/src/routes/meeting.ts
+++ src/api/src/routes/meeting.ts
@@ -146,25 +146,6 @@ export async function resendInvite(
 	});
 }
 
-export async function createCalendlyMeeting(data: any) {
-	const payload = data.payload;
-	const scheduledEvent = data.payload.scheduled_event;
-	const admins = await User.find({ role: ROLES.ADMIN });
-	let attendees = scheduledEvent.event_guests.map((userInfo: any) => userInfo.user_email);
-	attendees.push(...scheduledEvent.event_memberships.map((userInfo: any) => userInfo.user_email));
-	const doc = new meetingModel({
-		title: scheduledEvent.name,
-		start: scheduledEvent.start_time,
-		end: scheduledEvent.end_time,
-		members: setDifference(attendees,admins.map((admin: any) => admin.email)),
-		status: "active",
-		createdBy: payload.email,
-		externalEventId: scheduledEvent.uri.split("/").pop(),
-		joiningLink: scheduledEvent.location.join_url
-	});
-	await doc.save();
-}
-
 export const getMeetingList = async function (
 	req: express.Request,
 	res: express.Response
@@ -231,9 +212,40 @@ export const getMeeting = async function (
 	}
 };
 
+export async function createCalendlyMeeting(data: any) {
+	const payload = data.payload;
+	const scheduledEvent = data.payload.scheduled_event;
+	const admins = await User.find({ role: ROLES.ADMIN });
+	let attendees = scheduledEvent.event_guests.map((userInfo: any) => userInfo.user_email);
+	attendees.push(...scheduledEvent.event_memberships.map((userInfo: any) => userInfo.user_email));
+	const doc = new meetingModel({
+		title: scheduledEvent.name,
+		start: scheduledEvent.start_time,
+		end: scheduledEvent.end_time,
+		members: setDifference(attendees, admins.map((admin: any) => admin.email)),
+		status: "active",
+		createdBy: payload.email,
+		externalEventId: scheduledEvent.uri.split("/").pop(),
+		joiningLink: scheduledEvent.location.join_url
+	});
+	await doc.save();
+}
+
 export async function cancelCalendlyMeeting(data: any) {
-	console.log(JSON.stringify(data));
-	//const data_cancelled = {"created_at":"2023-12-24T17:55:40.000000Z","created_by":"https://api.calendly.com/users/f2f32253-5f99-4d22-8950-51f86535e227","event":"invitee.canceled","payload":{"cancel_url":"https://calendly.com/cancellations/3e39fe12-9338-448d-b760-2217a35cc0a7","cancellation":{"canceled_by":"Ashwani Kumar Verma","canceler_type":"host","created_at":"2023-12-24T17:55:39.915208Z","reason":""},"created_at":"2023-12-24T17:27:38.192438Z","email":"ashwaniverma.verma@rediff.com","event":"https://api.calendly.com/scheduled_events/9a15cc4a-c99d-48e4-bc2b-b363b35c669c","first_name":null,"invitee_scheduled_by":null,"last_name":null,"name":"test account","new_invitee":null,"no_show":null,"old_invitee":null,"payment":null,"questions_and_answers":[],"reconfirmation":null,"reschedule_url":"https://calendly.com/reschedulings/3e39fe12-9338-448d-b760-2217a35cc0a7","rescheduled":false,"routing_form_submission":null,"scheduled_event":{"cancellation":{"canceled_by":"Ashwani Kumar Verma","canceler_type":"host","created_at":"2023-12-24T17:55:39.915208Z","reason":""},"created_at":"2023-12-24T17:27:38.162964Z","end_time":"2023-12-24T21:45:00.000000Z","event_guests":[],"event_memberships":[{"user":"https://api.calendly.com/users/f2f32253-5f99-4d22-8950-51f86535e227","user_email":"connect2ashwaniverma@gmail.com"}],"event_type":"https://api.calendly.com/event_types/ebd636e1-bf06-4b5b-a3f8-68add9964c7a","invitees_counter":{"total":1,"active":0,"limit":100},"location":{"join_url":"https://calendly.com/events/9a15cc4a-c99d-48e4-bc2b-b363b35c669c/google_meet","status":"pushed","type":"google_conference"},"name":"15 Minute Meeting","start_time":"2023-12-24T21:30:00.000000Z","status":"canceled","updated_at":"2023-12-24T17:55:39.924665Z","uri":"https://api.calendly.com/scheduled_events/9a15cc4a-c99d-48e4-bc2b-b363b35c669c"},"scheduling_method":null,"status":"canceled","text_reminder_number":null,"timezone":"Asia/Calcutta","tracking":{"utm_campaign":null,"utm_source":null,"utm_medium":null,"utm_content":null,"utm_term":null,"salesforce_uuid":null},"updated_at":"2023-12-24T17:55:39.938754Z","uri":"https://api.calendly.com/scheduled_events/9a15cc4a-c99d-48e4-bc2b-b363b35c669c/invitees/3e39fe12-9338-448d-b760-2217a35cc0a7"}};
+	const payload = data.payload;
+	const scheduledEvent = data.payload.scheduled_event;
+	await meetingModel.updateOne(
+		{
+			externalEventId: scheduledEvent.uri.split("/").pop(),
+			createdBy: payload.email,
+		},
+		{
+			$set: {
+				status: "inactive",
+				modifiedAt: Date.now()
+			}
+		}
+	);
 }
 
 export async function meetingWebhook(
